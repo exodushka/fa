e16cdba9b806b24f3ef397e36b88d45a3fa4161d
exodushka <topiterh@gmail.com>
2025-11-05 12:30:50 +0300

    q

--- pythonalg/free.py
+++ pythonalg/free.py
@@ -1,5 +1,27 @@
-import matplotlib.pyplot as plt
-x = [1,2,3]
-y = [3, 4,5]
-plt.plot(x,y)
-plt.show()
+def clicker_solver(up : int, goal : int) -> int:
+    if up == 0 or goal == 0:
+        if up == 0 and goal == 0:
+            return 0
+        if up == 0 and goal != 0:
+            return -1
+        if up != 0 and goal == 0:
+            return 0
+    upgrade_cost = up ** 3
+    count = 0
+    clicks = 0
+    while count < goal:
+        while (count < upgrade_cost and count < goal) and (not(count == goal)):
+            count += up
+            clicks += 1
+            print(count, clicks, up)
+        if count >= goal:
+            return clicks
+        if ((goal - count) / up) > ((goal - (count - upgrade_cost)) / (up*2)):
+            count -= upgrade_cost
+            up *= 2
+            upgrade_cost = up **3
+            clicks += 1
+    return clicks
+    raise NotImplementedError
+
+print(clicker_solver(5,3030)) # 11
--- pythonalg/games/bull_and_cows/main.py
+++ pythonalg/games/bull_and_cows/main.py
@@ -9,7 +9,6 @@ def game():
             int(user_number)
             char1, char2, char3, char4 = user_number[0], user_number[1], user_number[2], user_number[3]
             if (user_number.count(char1) + user_number.count(char2) + user_number.count(char3) + user_number.count(char4)) != 0:
-
                 bulls = (secret_number[0] == char1) + (secret_number[1] == char2) + (secret_number[2] == char3) + (secret_number[3] == char4)
                 #cows = (secret_number.count(char1)) + (secret_number.count(char2)) + (secret_number.count(char3)) + (secret_number.count(char4)) - bulls
                 #cows = (char1 in secret_number and char1 != secret_number[0]) + (char2 in secret_number and char2 != secret_number[1]) + (char3 in secret_number and char3 != secret_number[2]) + (char4 in secret_number and char4 != secret_number[3])
